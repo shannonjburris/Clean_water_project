ff073d63eb82fed5fda073c43b1e26141bbcaa4d
Shannon Burris <shannonjburris@gmail.com>
2021-08-04 12:27:14 -0400

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,41 @@
-s
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection'); 
+
+class Post extends Model {
+  
+}
+
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+      body: {
+          type: DataTypes.STRING,
+          allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.STRING,
+        references: {
+            model: "user",
+            key: id,
+        },
+    }
+    },
+    {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+}
+);
+
+    module.exports = Post;
