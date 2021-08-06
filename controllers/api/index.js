670cbadcdab1d0cbe703eb353dc3ae4175ce5fab
Shannon <shannonjburris@gmail.com>
2021-08-06 14:28:49 -0400

    county.handlebars added, nav bar info added.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const userRoute = require('./user-routes');
 const postRoute = require('./post-routes');
 // const commentRoute = require('./comment-routes');
+const pictureRoute = require
 
 router.use('/users', userRoute);
 router.use('/post', postRoute);
--- controllers/api/picture-routes.js
+++ controllers/api/picture-routes.js
@@ -1,18 +1,20 @@
 const router = require("express").Router();
 const path = require('path');
 // id and key go here
+const access_id = "AKIAYFNZHDQTGQ3KXSFG";
+const access_key = "0/J4LD8v0U4JfAwAara2GEQLwGUG0Ze/CYDScY+Y";
 
-var aws = require('aws-sdk')
-var multer = require('multer')
-var multerS3 = require('multer-s3')
-var app = express()
-var s3 = new aws.S3({
+const aws = require('aws-sdk')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const app = express()
+const s3 = new aws.S3({
   accessKeyId: access_id,
   secretAccessKey: access_key,
   region: 'us-east-2'
 })
 
-var upload = multer({
+const upload = multer({
     storage: multerS3({
         s3: s3,
         bucket: "cleanwater",
