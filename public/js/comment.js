ae929317a4232f3407851da61c4297de1a571ceb
Shannon <shannonjburris@gmail.com>
2021-08-07 14:30:25 -0400

    comment bug fixed and some of picture-route

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,8 +2,9 @@ const submitCommentForm = async (event) =>{
     event.preventDefault();
 
     const body = document.querySelector('#comment-form').value.trim();
-    console.log(event)
-    const response = await fetch('/api/comment', {
+    const id   = document.querySelector('.comment-submit').getAttribute('id');
+    
+    const response = await fetch('/api/comment/' + id, {
         method: 'POST',
         body: JSON.stringify({body}),
         headers: { 'Content-Type' : 'application/json' }
